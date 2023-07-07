301b52831e633301e5c9d7fe3be05e1c6fb97a18
makcc666 <makcc6666@gmail.com>
2023-07-08 02:42:49 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import * as process from "process";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
+
+async function start() {
+  const timeStart = Date.now();
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(
+    PORT,
+    () => Logger.log(`Server start on port "${PORT}", "${Date.now() - timeStart}" ms`)
+  );
+}
+
+start();
