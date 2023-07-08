12b74f701a5e193e4bda241125cf19cc69659395
makcc666 <makcc6666@gmail.com>
2023-07-08 03:25:34 +0300

    init User + db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,26 @@
-import { Module } from "@nestjs/common";
-import { SequelizeModule } from "@nestjs/sequelize";
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from "@nestjs/config";
-import * as process from "process";
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
+import { User } from './users/user.model';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath:`.${process.env.NODE_ENV}.env`,
-    }),
-     SequelizeModule.forRoot({
-       dialect: 'postgres',
-       host: process.env.POSTGRES_HOST,
-       port: +process.env.POSTGRES_PORT,
-       username: process.env.POSTGRES_USER,
-       password: process.env.POSTGRES_PASSWORD,
-       database: process.env.POSTGRES_DB,
-       models: [],
-       autoLoadModels:true,
-     }),
-     UsersModule
-  ]
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`,
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: +process.env.POSTGRES_PORT,
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			models: [User],
+			autoLoadModels: true,
+		}),
+		UsersModule,
+	],
 })
 export class AppModule {}
